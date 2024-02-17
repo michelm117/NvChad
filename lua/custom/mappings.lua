5a316cae16f331e82f501f532d8a47537b7d7672
Marc Michel <marc@michel.lu>
2024-02-17 19:11:42 +0100

    feat: add golang support

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -8,7 +8,7 @@ M.undotree = {
   }
 }
 
-M.tagbar= {
+M.tagbar = {
   n = {
     ["<leader>t"] = { "<cmd> TagbarToggle <CR>", "Togggle tag bar" },
   }
@@ -17,7 +17,7 @@ M.tagbar= {
 M.nvimtree = {
   n = {
     ["<C-b>"] = { "<cmd> NvimTreeToggle <CR>", "Toggle nvimtree" },
-    ["<C-n>"] = { "<nil>", ""},
+    ["<C-n>"] = { "<nil>", "" },
   },
 }
 
@@ -26,5 +26,57 @@ M.telescope = {
     ["<leader>fg"] = { "<cmd> Telescope live_grep <CR>", "Live grep" },
   },
 }
+
+
+M.dap = {
+  plugin = true,
+  n = {
+    ["<leader>db"] = {
+      "<cmd> DapToggleBreakpoint <CR>",
+      "Add breakpoint at line"
+    },
+    ["<leader>dus"] = {
+      function()
+        local widgets = require('dap.ui.widgets');
+        local sidebar = widgets.sidebar(widgets.scopes);
+        sidebar.open();
+      end,
+      "Open debugging sidebar"
+    }
+  }
+}
+
+M.dap_go = {
+  plugin = true,
+  n = {
+    ["<leader>dgt"] = {
+      function()
+        require('dap-go').debug_test()
+      end,
+      "Debug go test"
+    },
+    ["<leader>dgl"] = {
+      function()
+        require('dap-go').debug_last()
+      end,
+      "Debug last go test"
+    }
+  }
+}
+
+M.gopher = {
+  plugin = true,
+  n = {
+    ["<leader>gsj"] = {
+      "<cmd> GoTagAdd json <CR>",
+      "Add json struct tags"
+    },
+    ["<leader>gsy"] = {
+      "<cmd> GoTagAdd yaml <CR>",
+      "Add yaml struct tags"
+    }
+  }
+}
+
 
 return M
